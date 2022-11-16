1dc06f5637d1cf6ea23a1a91ca9444d40c839b99
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2022-11-16 21:26:24 +0600

    models added

--- Controllers/loginCheck.php
+++ Controllers/loginCheck.php
@@ -1,6 +1,8 @@
 <?php
 
     session_start();
+    require_once "../models/userModel.php";
+
     $username = $_POST['username'];
     $password = $_POST['password'];
     $usertype = $_POST['usertype'];
@@ -32,44 +34,6 @@
         //header('location: login.php');
     }
 /*
-    if ($username == "" || $password == "" || $usertype == "") {
-        header('location: login.php?err=null');
-    } else {
-        $myUser = fopen('users.txt', "r");
-        while (!feof($myUser)) {
-            $data = fgets($myUser);
-            $user = explode(" | ", $data);
-            if ($user[0] == $username && $user[1] == $password && $user[2] == $usertype) {
-                if(strlen($username)<2) {
-                    setcookie('status', 'true', time() + 3000, '/');
-                    if($usertype == 'admin') {
-                        header('location: adminHome.php');
-                    } else if($usertype == 'employee') {
-                        header('location: empHome.php');
-                    } else if($usertype == 'customer') {
-                        header('location: cusHome.php');
-                }
-                } else if((substr_count($username,'@')>0) || (substr_count($username,'#')>0) || (substr_count($username,'$')>0) || (substr_count($username,'%')>0) || (substr_count($username,'/')>0) || (substr_count($username,'*')>0) || (substr_count($username,'+')>0) || (substr_count($username,'(')>0) || (substr_count($username,')')>0) || (substr_count($username,'!')>0) || (substr_count($username,'^')>0)) {
-                        setcookie('status', 'true', time() + 3000, '/');
-                    if($usertype == 'admin') {
-                        header('location: adminHome.php');
-                    } else if($usertype == 'employee') {
-                        header('location: empHome.php');
-                    } else if($usertype == 'customer') {
-                        header('location: cusHome.php');
-                    }
-                }
-                
-                //header('location: cusHome.php');
-            }
-        }
-        echo "<h2> 
-        Invalid User <br>
-        Please provide correct login information
-        </h2>";
-        //header('location: login.php');
-    }
-////
     if($username == "" || $password == "" ){
         echo "<h2> Validation failed: Username or Password is missing! </h2>";
     }
--- Models/userModel.php
+++ Models/userModel.php
@@ -1,21 +1,53 @@
 <?php
 
-  require_once('databaseModel.php');
-  session_start();
-
+  require_once "databaseCon.php";
+  
   //To check users DB to login
-  function validate($username, $password){
+  function login($user){
     $con = getConnection();
-    $sql = "SELECT * FROM users WHERE username='{$username}' and password='{$password}'";
-
+    $sql = "select * from users where username='{$user['username']}' and password='{$user['password']}'";
     $result = mysqli_query($con, $sql);
     $user = mysqli_fetch_assoc($result);
-
     if($user != null){
-      return true;
-    } else{
-      return false;
+        return true;
+    }else{
+        return false;
     }
+  }
+
+  function insertUser($user){
+    $con = getConnection();
+    $sql = "INSERT into users VALUES('', '{$user['username']}', '{$user['password']}', '{$user['email']}', '{$user['email']}', '{$user['email']}')";
+    $status = mysqli_query($con, $sql);
+    return $status;
+  }
+
+  function getAllUser(){
+    $con = getConnection();
+    $sql = "SELECT * from users";
+    $status = mysqli_query($con, $sql);
+    return $status;
+  }
+  
+  function editUser(){
+    $con = getConnection();
+    $sql = "UPDATE users SET('{$user['username']}', '{$user['password']}', '{$user['email']}')";
+    $status = mysqli_query($con, $sql);
+    return $status;
+  }
+  
+  function deleteUser(){
+    $con = getConnection();
+    $sql = "DELETE FROM users WHERE('{$user['id']}')";
+    $status = mysqli_query($con, $sql);
+    return $status;
+  }
+  
+  function getUserByID(){
+    $con = getConnection();
+    $sql = "SELECT * from users WHERE({$user['id']})";
+    $status = mysqli_query($con, $sql);
+    return $status;
   }
 
 ?>
